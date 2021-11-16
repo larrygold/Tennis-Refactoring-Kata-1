4113a894d1ebd0b1afe26cec14263bd4241d4e74
Larry Goldstein <larry@goldstein.fr>
2021-11-16 18:45:28 +0100

    Update TennisGame1.cs

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -32,53 +32,56 @@ namespace Tennis
                 }
             }
 
-            var score = "";
             if (IsAdvantageOrWin())
             {
                 var scoresDelta = Math.Abs(_scorePlayerOne - _scorePlayerTwo);
                 var leadingPlayerName = _scorePlayerOne > _scorePlayerTwo ? "player1" : "player2";
 
                 if (scoresDelta == 1)
-                {
                     return $"Advantage {leadingPlayerName}";
-                }
-                else
-                {
-                    return $"Win for {leadingPlayerName}";
-                }
 
-                /*
-                var minusResult = _scorePlayerOne - _scorePlayerTwo;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
-            */
+                return $"Win for {leadingPlayerName}";
             }
-            else
+
+            var score = "";
+            var tempScore = 0;
+
+            tempScore = _scorePlayerOne;
+            switch (tempScore)
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    var tempScore = 0;
-                    if (i == 1) tempScore = _scorePlayerOne;
-                    else { score += "-"; tempScore = _scorePlayerTwo; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
+
+            score += "-";
+
+            tempScore = _scorePlayerTwo;
+            switch (tempScore)
+            {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+
             return score;
         }
 
