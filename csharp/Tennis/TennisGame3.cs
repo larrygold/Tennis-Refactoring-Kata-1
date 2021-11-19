88c07ac4e612d3f359d2deff64c222cd2de816ca
Larry Goldstein <larry@goldstein.fr>
2021-11-19 11:50:30 +0100

    Created Player class

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,16 +2,26 @@ using System;
 
 namespace Tennis
 {
+    public class Player
+    {
+        public int Points { get; set; }
+        public string Name { get; private set; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+
     public class TennisGame3 : ITennisGame
     {
         private int _player2Points;
-        private int _player1Points;
-        private string _player1Name;
         private string _player2Name;
+        private readonly Player _player;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            _player1Name = player1Name;
+            _player = new Player(player1Name);
             _player2Name = player2Name;
         }
 
@@ -19,15 +29,15 @@ namespace Tennis
         {
             var individualScores = new string[] { "Love", "Fifteen", "Thirty", "Forty" };
             var scorePlayer1 = "";
-            if (_player1Points <= 3)
+            if (_player.Points <= 3)
             {
-                scorePlayer1 = individualScores[_player1Points];
+                scorePlayer1 = individualScores[_player.Points];
             }
 
 
             if (IsEquality())
             {
-                if (_player1Points < 3)
+                if (_player.Points < 3)
                 {
                     return scorePlayer1 + "-All";
                 }
@@ -35,41 +45,43 @@ namespace Tennis
                 return "Deuce";
             }
 
-            if ((_player1Points < 4 && _player2Points < 4) &&(_player1Points + _player2Points < 6))
+            if (_player.Points < 4 && _player2Points < 4 && _player.Points + _player2Points < 6)
             {
                 var scorePlayer2 = individualScores[_player2Points];
                 return scorePlayer1 + "-" + scorePlayer2;
             }
-            else
+
+            if (_player.Points >= 4 || _player2Points >= 4 || _player.Points + _player2Points >= 6)
             {
-                if (_player1Points > _player2Points)
+                if (_player.Points > _player2Points)
                 {
-                    scorePlayer1 = _player1Name;
+                    scorePlayer1 = _player.Name;
                 }
                 else
                 {
                     scorePlayer1 = _player2Name;
                 }
 
-                if ((_player1Points - _player2Points) * (_player1Points - _player2Points) == 1)
+                if ((_player.Points - _player2Points) * (_player.Points - _player2Points) == 1)
                 {
                     return "Advantage " + scorePlayer1;
                 }
 
                 return "Win for " + scorePlayer1;
-
             }
+
+            throw new Exception();
         }
 
         private bool IsEquality()
         {
-            return _player1Points == _player2Points;
+            return _player.Points == _player2Points;
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == _player1Name)
-                _player1Points ++;
+            if (playerName == _player.Name)
+                _player.Points ++;
             else if (playerName == _player2Name)
                 _player2Points ++;
             else
